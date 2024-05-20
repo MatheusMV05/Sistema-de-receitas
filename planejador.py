560c9d1233c336d671d72ab12242225efb419d62
Cláudia <mcrcoa@cesar.school>
2024-05-19 23:37:30 -0300

    novo código da função planejador de refeições

--- planejador.py
+++ planejador.py
@@ -1,30 +1,28 @@
-def planejador_da_semana(plano_semanal, dia_da_semana, refeicao, receita):
-    if dia_da_semana not in plano_semanal:
-        plano_semanal[dia_da_semana] = {}
-    if refeicao not in plano_semanal[dia_da_semana]:
-        plano_semanal[dia_da_semana][refeicao] = []
-    plano_semanal[dia_da_semana][refeicao].append(receita)
-
-receitas = {'Frango assado': {'frango': 1, 'limão': 2, 'alho': 3, 'azeite': 1},
+def planejador_de_refeicoes():
+    planejador = []
+    receitas = {
+    'Frango assado': {'frango': 1, 'limão': 2, 'alho': 3, 'azeite': 1},
     'Macarrão com molho de tomate': {'macarrão': 1, 'tomate': 4, 'cebola': 1, 'alho': 2},
-    'Salada Caesar': {'alface': 1, 'croutons': 1, 'parmesão': 0.5, 'molho Caesar': 1},}
-
-def planejador_de_receitas():
-    plano_semanal = {}
+    'Salada Caesar': {'alface': 1, 'croutons': 1, 'parmesão': 0.5, 'molho Caesar': 1}
+}
     while True:
-        opcao = input("Deseja adicionar uma receita ao plano semanal (sim ou não)? : ")
+        opcao = input("Deseja adicionar uma receita ao plano de refeições (sim ou não)? : ")
         if opcao != 'sim':
             break
-        dia_da_semana = input("Digite o dia da semana (por exemplo, Segunda-feira): ")
+
         refeicao = input("Digite a refeição (por exemplo, Café da manhã, Almoço, Jantar): ")
         receita = input("Digite o nome da receita: ")
-        if receita in receitas:
-            planejador_da_semana(plano_semanal, dia_da_semana, refeicao, receita)
-        else:
-            print("Receita não encontrada")
+        with open("receitas.txt", "r") as file:
+            file.read
+            if receita in receitas:
+                planejador_de_refeicoes(refeicao, receita)
+            else:
+                print("Receita não encontrada")
+        planejador.append(receita)
+        with open("planejador.txt", "w") as file:
+            file.write(f"Refeição: {refeicao}; Receita{receita}")
 
-    print("\nPlano Semanal:")
-    for dia in plano_semanal:
-        print(f"\n{dia}:")
-        for refeicao in plano_semanal[dia]:
-            print(f"{refeicao}: {', '.join(plano_semanal[dia][refeicao])}")
+    print("\nPlanejador de Refeições:")
+    for refeicao in planejador_de_refeicoes:
+        print(f"Refeição: {refeicao}")
+        print(f"Receita: {receita}")
