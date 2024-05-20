33016f8186d4f48e21d569794f2bc04e53f25f67
Maria-ClaudiaA <162516679+Maria-ClaudiaA@users.noreply.github.com>
2024-05-20 14:45:36 -0300

    adaptei o código para funcionar melhor no código principal

--- planejador.py
+++ planejador.py
@@ -1,28 +1,20 @@
-def planejador_de_refeicoes():
+def planejador_de_refeicoes(receitas):
     planejador = []
-    receitas = {
-    'Frango assado': {'frango': 1, 'limão': 2, 'alho': 3, 'azeite': 1},
-    'Macarrão com molho de tomate': {'macarrão': 1, 'tomate': 4, 'cebola': 1, 'alho': 2},
-    'Salada Caesar': {'alface': 1, 'croutons': 1, 'parmesão': 0.5, 'molho Caesar': 1}
-}
     while True:
         opcao = input("Deseja adicionar uma receita ao plano de refeições (sim ou não)? : ")
-        if opcao != 'sim':
+        if opcao.lower()!= 'sim':
             break
-
-        refeicao = input("Digite a refeição (por exemplo, Café da manhã, Almoço, Jantar): ")
-        receita = input("Digite o nome da receita: ")
-        with open("receitas.txt", "r") as file:
-            file.read
-            if receita in receitas:
-                planejador_de_refeicoes(refeicao, receita)
-            else:
-                print("Receita não encontrada")
-        planejador.append(receita)
-        with open("planejador.txt", "w") as file:
-            file.write(f"Refeição: {refeicao}; Receita{receita}")
-
+        else:
+            refeicao = input("Digite a refeição (por exemplo, Café da manhã, Almoço, Jantar): ")
+            receita_nome = input("Digite o nome da receita: ")
+            for receita in receitas:
+                if receita["nome"] == receita_nome:
+                    planejador.append({"refeicao": refeicao, "receita": receita_nome})
+                    with open("planejador.txt", "a") as file:
+                        file.write(f"Refeição: {refeicao}; Receita: {receita_nome}\n")
+                    print("Receita adicionada ao plano de refeições com sucesso!")
+                    break
     print("\nPlanejador de Refeições:")
-    for refeicao in planejador_de_refeicoes:
-        print(f"Refeição: {refeicao}")
-        print(f"Receita: {receita}")
+    with open("planejador.txt", "r") as file:
+        for line in file:
+            print(line.strip())
